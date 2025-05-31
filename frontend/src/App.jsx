5dca8a4983478241e2a47d1204d18da73f38b74f
IsacJunio <isacsac.bh@gmail.com>
2025-05-31 12:53:02 -0300

    Fix: Corringi a build do app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
-import "./Login.css";
 import Api from "../../services/UserLogin.js";
 import { AuthContext } from "../../App"; // Importa o contexto
 
